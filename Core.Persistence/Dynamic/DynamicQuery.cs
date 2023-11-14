4ba79fc119f837b53716a5a7bebb2e1edfa0c91c
Deniz Türk <deniz.turk@ogr.gsu.edu.tr>
2023-11-14 23:07:06 +0300

    Add project files.

---
+++ Core.Persistence/Dynamic/DynamicQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Dynamic
+{
+    public class DynamicQuery
+    {
+
+        public DynamicQuery()
+        {
+
+        }
+
+        public DynamicQuery(IEnumerable<Sort>? sort, Filter? filter)
+        {
+            Filter = filter;
+            Sort = sort;
+        }
+
+
+        public IEnumerable<Sort>? Sort { get; set; }
+        public Filter? Filter { get; set; }
+
+ 
+    }
+}
---
+++ Core.Persistence/Dynamic/Filter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Dynamic
+{
+    public class Filter
+    {
+        public Filter()
+        {
+            Field = string.Empty;
+            Operator = string.Empty;
+        }
+
+        public Filter(string field, string @operator)
+        {
+            Field = field;
+            Operator = @operator;
+        }
+        public String Field { get; set; }
+        public String? Value { get; set; }
+
+        public String Operator { get; set; }
+        public String? Logic { get; set; }
+        public IEnumerable<Filter> Filters { get; set; }
+
+     
+    }
+}
